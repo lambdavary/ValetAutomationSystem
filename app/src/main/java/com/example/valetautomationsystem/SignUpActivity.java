85b65fa1fbde67214de9f17c9fffc062052bd8b6
lambdavary <yilmaz.sanli@agu.edu.tr>
2019-04-28 15:40:50 +0300

    Second Commit

--- app/src/main/java/com/example/valetautomationsystem/SignUpActivity.java
+++ app/src/main/java/com/example/valetautomationsystem/SignUpActivity.java
@@ -1,11 +1,17 @@
 package com.example.valetautomationsystem;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
@@ -24,17 +30,19 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         mAuth = FirebaseAuth.getInstance();
 
-
         usernameField = findViewById(R.id.usernameField);
         emailField = findViewById(R.id.emailField);
         passwordField = findViewById(R.id.passwordField);
         passwordCheckField = findViewById(R.id.userPasswordCheckField);
         signUpButton  = findViewById(R.id.userSignUpButton);
+        signUpButton.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == signUpButton.getId()){
+            Log.v("sifre", passwordField.getText().toString());
             signUp();
         }
     }
@@ -43,8 +51,18 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         String email = emailField.getText().toString();
         String password = passwordField.getText().toString();
 
+        Log.v("password", password);
+
         try {
-            mAuth.createUserWithEmailAndPassword(email, password);
+            mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(), "yeyy", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
         }catch (Exception e){
             e.printStackTrace();
         }
